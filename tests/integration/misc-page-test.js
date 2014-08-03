87733445adba905dff6a12b8973d6c10bbc3d404
Timothy Warrington <tim.warrin@gmail.com>
2014-08-03 14:39:34 -0600

    add new integration tests

---
+++ tests/integration/misc-page-test.js
@@ -0,0 +1,50 @@
+import startApp from '../helpers/start-app';
+
+var App;
+
+module('Integration - Misc Page', {
+  setup: function() {
+    App = startApp();
+  },
+  teardown: function() {
+    App.reset();
+  }
+});
+
+test('Should navigate to the Misc page', function() {
+  visit('/').then(function() {
+    click("a:contains('Misc')").then(function() {
+      equal(find('h1.logo').text(), 'CS:GO  Configr');
+      equal(find('div.instructions').find('h3').text(), 'Instructions');
+      equal(find('section').eq(0).find('h3').text(), 'Miscellaneous');
+      equal(find('section').eq(1).find('h3').text(), 'Preview');
+    });
+  });
+});
+
+test('Should close instructions', function() {
+  visit('/n/misc').then(function() {
+    click('a.instructions--close').then(function() {
+      notEqual(find('h3').text(), 'Instructions');
+    });
+  });
+});
+
+test('Should download cfg file', function() {
+  visit('/n/misc').then(function() {
+    click('button.btn--download').then(function() {
+      expect(0);
+      console.log('File should download, any other way to test this?');
+    });
+  });
+});
+
+test('Should download cfg file and redirect to show page', function() {
+  visit('/n/misc').then(function() {
+    click('button.btn--download__save').then(function() {
+      console.log('File should download, any other way to test this?');
+      equal(find('h1.logo').text(), 'CS:GO  Configr');
+      equal(find('section').eq(0).find('h3').text(), 'Preview');
+    });
+  });
+});
---
+++ tests/integration/video-page-test.js
@@ -0,0 +1,50 @@
+import startApp from '../helpers/start-app';
+
+var App;
+
+module('Integration - Video Page', {
+  setup: function() {
+    App = startApp();
+  },
+  teardown: function() {
+    App.reset();
+  }
+});
+
+test('Should navigate to the Video page', function() {
+  visit('/').then(function() {
+    click("a:contains('Video')").then(function() {
+      equal(find('h1.logo').text(), 'CS:GO  Configr');
+      equal(find('div.instructions').find('h3').text(), 'Instructions');
+      equal(find('section').eq(0).find('h3').text(), 'Video');
+      equal(find('section').eq(1).find('h3').text(), 'Preview');
+    });
+  });
+});
+
+test('Should close instructions', function() {
+  visit('/n/video').then(function() {
+    click('a.instructions--close').then(function() {
+      notEqual(find('h3').text(), 'Instructions');
+    });
+  });
+});
+
+test('Should download cfg file', function() {
+  visit('/n/video').then(function() {
+    click('button.btn--download').then(function() {
+      expect(0);
+      console.log('File should download, any other way to test this?');
+    });
+  });
+});
+
+test('Should download cfg file and redirect to show page', function() {
+  visit('/n/video').then(function() {
+    click('button.btn--download__save').then(function() {
+      console.log('File should download, any other way to test this?');
+      equal(find('h1.logo').text(), 'CS:GO  Configr');
+      equal(find('section').eq(0).find('h3').text(), 'Preview');
+    });
+  });
+});
