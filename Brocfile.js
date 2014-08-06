dc94ab0210e0ef25c46152ce9f0f8de7efec1614
Timothy Warrington <tim.warrin@gmail.com>
2014-08-06 13:05:01 -0600

    add inuit bower, broccoli sass, reorganize ui scss files

--- Brocfile.js
+++ Brocfile.js
@@ -3,6 +3,10 @@
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 var app = new EmberApp({
+  minifyCSS: {
+    enabled: true,
+    options: {}
+  },
   fingerprint: {
     exclude: ['images']
   }
