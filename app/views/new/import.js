274aee38e9fee26b674284bba0c4e3d9728cbe06
Timothy Warrington <tim.warrin@gmail.com>
2014-09-14 22:00:26 -0600

    move parser into import controller and view

--- app/views/new/import.js
+++ app/views/new/import.js
@@ -1,6 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.View.extend({
+  didInsertElement: function() {
+    this.set('controller.inputFile', Ember.$('input[data-upload="file"]')[0]);
+    this.set('controller.inputText', Ember.$('textarea[data-upload="text"]')[0]);
+  },
+  bindEvents: function() {
+    this.get('controller.inputFile').on('change', Ember.$.proxy(this.get('controller.doParse'), this));
+    this.get('controller.inputText').on('input', Ember.$.proxy(this.get('controller.doParse'), this));
+  }.observes('didInsertElement'),
   showHideConfigImport: function() {
     var $importOptions = Ember.$('input#fileImport, input#textImport'),
         $fileRadio = Ember.$('input#fileImport'),
