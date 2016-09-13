7dff14b4ef56112527c3e4521d65c4a6f449be1f
haskly <haskly@users.noreply.github.com>
2016-09-13 12:29:13 -0600

    add style prefixing, update component layouts

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,6 +4,10 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
+    autoprefixer: {
+      browsers: ['last 2 ios version'],
+      cascade: false
+    },
     minifyCSS: {
       enabled: true,
       options: {}
